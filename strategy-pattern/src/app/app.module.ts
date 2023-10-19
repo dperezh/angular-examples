13cd9a52e228212ec947c7774ecdb0541c802ca6
Daniel Perez <dperez@soaint.com>
2023-10-19 12:59:44 -0400

    refactor: move code to the corresponding components

--- strategy-pattern/src/app/app.module.ts
+++ strategy-pattern/src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StrategyComponent } from './components/strategy/strategy.component';
+import { RoutingModule } from './router/routing/routing.module';
 import { FileService } from './services/file.service';
+import { SignalsComponent } from './components/signals/signals.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StrategyComponent,
+    SignalsComponent,
+    PresentationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [FileService],
   bootstrap: [AppComponent]
